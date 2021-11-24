229acfc588131128c24d9af7c4fb35be1b397d6a
nylee <90261873+leenayeon1@users.noreply.github.com>
2021-11-24 21:49:37 +0900

    Update logout.js

--- routes/logout.js
+++ routes/logout.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res) {
   req.session.destroy(function (err) {
     console.log("로그아웃");
-    res.render("login");
+    res.render("login"); //로그인 창으로 이동
     console.log(req.session);
     });
 });
