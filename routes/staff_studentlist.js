a08613e49c895a20c04d9a710b4a574caad31488
nylee <90261873+leenayeon1@users.noreply.github.com>
2021-11-25 01:51:31 +0900

    Update staff_studentlist.js

--- routes/staff_studentlist.js
+++ routes/staff_studentlist.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var connection = require("../config/db"); //디비 사용위해 필요, connection.query()식으로 사용
 
 router.get("/", function (req, res) {
   if (req.session.uid) {
     res.render("staff_studentlist");
+     /*
+    여기에 render("staff_studentlist", { data: context, data1: context1 }) 이런식으로 데이터 넘겨주는 쿼리 작성,
+    쿼리 작성끝내면 위의 res.render("staff_studentlist");문은 지워야함
+    */
+    
   } else {
     res.write(
       "<script type='text/javascript'>alert('Please log in');</script>"
