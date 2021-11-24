3c62325e3b373cf18519cc34e3d158b74cfb1862
nylee <90261873+leenayeon1@users.noreply.github.com>
2021-11-25 01:56:04 +0900

    Update student_attendance.js

--- routes/student_attendance.js
+++ routes/student_attendance.js
@@ -9,8 +9,8 @@ router.get("/", function (req, res) {
   if (req.session.uid) {
     res.render("student_attendance");
     /*
-    여기에 render("student_attendance", { data: context, data1: context1 }) 이런식으로 데이터 넘겨주는 쿼리 작성,
-    위의 res.render("student_attendance");문은 지워야함
+    여기에 render("student_attendance", { data: context, data1: context1 }) 이런식으로 데이터 넘겨주는 쿼리 작성
+    위의 res.render("student_attendance");문은 지워야함("student_attendance"은 student_attendance.ejs를 말하는 것임, student_attendance.ejs페이지를 렌더링한다는 뜻)
     */
     
   } else {
