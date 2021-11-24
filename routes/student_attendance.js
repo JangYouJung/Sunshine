0ce83bb2c152e72e8caba51618a65ff93a7c50a1
nylee <90261873+leenayeon1@users.noreply.github.com>
2021-11-25 01:53:07 +0900

    Update student_attendance.js

--- routes/student_attendance.js
+++ routes/student_attendance.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var connection = require("../config/db"); //디비 사용위해 필요, connection.query()식으로 사용
 
 router.get("/", function (req, res) {
   console.log(req.session);
 
   if (req.session.uid) {
     res.render("student_attendance");
+    /*
+    여기에 render("student_attendance", { data: context, data1: context1 }) 이런식으로 데이터 넘겨주는 쿼리 작성,
+    위의 res.render("student_attendance");문은 지워야함
+    */
+    
   } else {
        res.write(
          "<script type='text/javascript'>alert('Please log in');</script>"
