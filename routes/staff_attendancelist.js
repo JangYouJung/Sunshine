88cac135717d367edfab52a694790f25e316bd50
nylee <90261873+leenayeon1@users.noreply.github.com>
2021-11-25 01:47:34 +0900

    Update staff_attendancelist.js

--- routes/staff_attendancelist.js
+++ routes/staff_attendancelist.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var connection = require("../config/db"); //디비 사용위해 필요, connection.query()식으로 사용
 
 router.get("/", function (req, res) {
   
     if (req.session.uid) {
       res.render("staff_attendancelist");
+       /*
+    여기에 render("staff_attendancelist", { data: context, data1: context1 }) 이런식으로 데이터 넘겨주는 쿼리 작성
+    */
+      
     } else {
       res.write(
         "<script type='text/javascript'>alert('Please log in');</script>"
